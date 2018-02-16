f602cdd2e994837e635ee9682df57c8d5a10c709
unknown <claudioguion@gmail.com>
2018-02-16 10:17:33 -0400

    Practica con matrices en python

---
+++ matrizprueba.py
@@ -0,0 +1,13 @@
+ncolums = 2
+nfilas = 4
+matriz = [[0] * ncolums for i in range(nfilas)]
+matriz[0][0] = "Pera"
+matriz[0][1] = "Manzana"
+matriz[1][0] = "Naranja"
+matriz[1][1] = "Fresa"
+matriz[2][0] = "Patilla"
+matriz[2][1] = "Uva"
+matriz[3][0] = "Mandarina"
+matriz[3][1] = "Kiwi"
+
+print(matriz)
