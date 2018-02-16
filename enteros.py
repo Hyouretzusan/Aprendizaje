a07a8062ecb0ca54be118c6fa6a31edac615b09d
unknown <claudioguion@gmail.com>
2018-02-16 10:41:47 -0400

    Practica con int en python

---
+++ enteros.py
@@ -0,0 +1,19 @@
+a = 26
+b = 11.3
+c = 5
+d = 0.5
+
+# SUMA
+print(a + b)
+#RESTA
+print(a - c)
+#MULTIPLICACIóN
+print(a * d)
+#EXPONENTE
+print(b ** 5)
+#DIVISIóN
+print(a / c)
+#MóDULO
+print(b % d)
+
+print(float(c))
