c0f9526533ff97c8bf9e460bda93a50dc5734072
unknown <claudioguion@gmail.com>
2018-02-20 12:11:28 -0400

    Practica con decoradores y excepciones en python

---
+++ exceptions.py
@@ -0,0 +1,40 @@
+#Ejercicio generator function
+def get_odds(rangUsu = 10):
+    genOdd = (number for number in range(rangUsu) if number % 2 != 0)
+    return(genOdd)
+count = 0
+for numberOdd in get_odds():
+    if count == 3:
+        print("Get odds:",numberOdd)
+    count += 1
+
+print("\n")
+
+#funcion para prueba
+def good():
+    goodList = ["Harry", "Ron", "Hermione"]
+    return(goodList)
+
+#Ejercicio decorador
+def deco_funct(func):
+    print("Start")
+    print("The function passed:",func())
+    print("Finish")
+deco_funct(good)
+
+print("\n")
+
+#Mi propia excepción
+class OopsException(Exception):
+    """docstring for Oops_Exception"""
+    pass
+
+words = ["5", "Darling", "4"]
+while True:
+    for word in words:
+        try:
+            float(word)
+            print("Float:",word)
+        except OopsException:
+            print("Caught an 'oops!'")
+            continue
