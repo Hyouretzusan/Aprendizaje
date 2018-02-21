5fb6bb35c67ab048e33284656032e3dd46421e27
unknown <claudioguion@gmail.com>
2018-02-21 01:11:21 -0400

    Practica con funcion zip

---
+++ ip_zip.py
@@ -0,0 +1,6 @@
+titles = ["Creature of habit", "Crewel fate"]
+plots = ["A nun turns into a monster", "A haunted yarn shop"]
+movies = {}
+for title, plot in zip(titles, plots):
+    movies[title] = plot
+print(movies)
