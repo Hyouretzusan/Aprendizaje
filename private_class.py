488985be4891eab8ba6ce5662843131b9c93a39d
unknown <claudioguion@gmail.com>
2018-02-22 10:51:56 -0400

    Practica con atributos privados para una clase en python

---
+++ private_class.py
@@ -0,0 +1,27 @@
+class Element():
+    """docstring for Element"""
+    def __init__(self, name, symbol, number):
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+
+    def get_(self):
+        print("Inside the getter")
+        return(self.__name, self.__symbol, self.__number)
+
+    def set_(self, name, symbol, number):
+        print("Inside the setter")
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+
+    name = property(get_, set_)
+    #symbol = property(get_, set_)
+    #number = property(get_, set_)
+
+elementDict = {"name": "Hydrogen", "symbol": "H", "number": "1"}
+elementDictList = list(elementDict.values())
+#print(elementDictList[0])
+
+hydrogen = Element(elementDictList[0], elementDictList[1], elementDictList[2])
+print(hydrogen.name)
