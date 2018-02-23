ad9a8c8ca9afa62d90e63e49da23a7aeb2e1f8fc
unknown <claudioguion@gmail.com>
2018-02-23 15:07:12 -0400

    Segundo commit. Usuario ingresa el titulo y el argumento

--- ip_zip.py
+++ ip_zip.py
@@ -1,6 +1,12 @@
 titles = ["Creature of habit", "Crewel fate"]
 plots = ["A nun turns into a monster", "A haunted yarn shop"]
+#ratings = ["5/10", "7/10"]
 movies = {}
-for title, plot in zip(titles, plots):
+for title, plot, rating in zip(titles, plots):
     movies[title] = plot
 print(movies)
+
+newMovie = input("Ingrese título de la película: ")
+append.titles(newMovie)
+newplot = input("Ingrese el argumento: ")
+append.plots(newplot)
