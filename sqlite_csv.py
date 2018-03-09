27af0be128799cb731c39196ba6346fa9cf3a58d
unknown <claudioguion@gmail.com>
2018-03-08 23:25:15 -0400

    Ejercicio para migrar datos desde un archivo csv hacia una base de datos sqlite

---
+++ sqlite_csv.py
@@ -0,0 +1,18 @@
+import csv
+import sqlite3
+from books_bdd import bdd_consultbooks, bdd_createtable
+
+conn = sqlite3.connect('books_db.sqlite')
+cur = conn.cursor()
+bdd_createtable()
+
+with open ("books.csv", "rt") as entrada:
+    csventrada = csv.DictReader(entrada)
+    
+    for book in csventrada:
+        #print(book["title"]) #DEBB
+        cur.execute('INSERT INTO Books (title, author, year) VALUES (?, ?, ?)', (book["title"], book["author"], book["year"]))
+        conn.commit()
+        print("Book registered: %s" % book["title"])
+
+bdd_consultbooks()
