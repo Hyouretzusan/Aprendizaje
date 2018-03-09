1b91057fb8fae5d80ba7f3737a6b480832602f00
unknown <claudioguion@gmail.com>
2018-03-08 23:31:57 -0400

    Primer commit

--- csvwritefiles_practica.py
+++ csvwritefiles_practica.py
@@ -1,6 +1,11 @@
 import csv
 
-villains = [['Doctor', 'No'],['Rosa', 'Klebb'],['Mister', 'Big'],['Auric', 'Goldfinger'],['Ernst', 'Blofeld'],]
+villains = [['Doctor', 'No'],
+['Rosa', 'Klebb'],
+['Mister', 'Big'],
+['Auric', 'Goldfinger'],
+['Ernst', 'Blofeld'],]
+
 with open('villains', 'wt') as fout:
     csvout = csv.writer(fout)
     csvout.writerows(villains)
